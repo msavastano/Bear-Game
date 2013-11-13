d266f090516449012afe4319eaad3f3c50b14226
msavastano <millhouse5000@gmail.com>
2013-11-13 09:30:24 -0500

    Create StateOfGame.cs

---
+++ StateOfGame.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BearGame
+{
+    public enum StateOfGame
+    {
+        Play,
+        Menu,
+        LevelChange
+    }
+}
